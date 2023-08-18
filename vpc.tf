f874a2dd4238de53b761ae75aed1ac6946fd41f0
lolakeem1 <141778874+lolakeem1@users.noreply.github.com>
2023-08-18 17:38:37 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -42,19 +42,12 @@ resource "aws_default_security_group" "default-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    description = "Allow all ingress traffic"
-    from_port = 9000
-    to_port = 9092
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
@@ -65,8 +58,20 @@ ingress {
     to_port     = 9092
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  } 
-
+  }  
+ingress {
+    from_port   = 8081
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow all ingress traffic"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -77,3 +82,4 @@ ingress {
     Name = "${var.env_prefix}-default-sg"
   }
 }
+  
